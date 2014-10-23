e3d831d28d756b7512af46970e0b2b4ec02a6870
LinYongW <linyongw@gmail.com>
2014-10-23 16:10:38 +0800

    modify

---
+++ JAVAConcurrent/src/main/java/cn/ly/concurrency/atomic/AtomicIntegerFieldUpdaterDemo.java
@@ -0,0 +1,30 @@
+package cn.ly.concurrency.atomic;
+
+import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
+
+public class AtomicIntegerFieldUpdaterDemo {
+
+	class DemoData{
+		public volatile int value1 = 1;
+		volatile int value2 = 2;
+		protected volatile int value3 = 3;
+		private volatile int value4 = 4;
+	}
+	AtomicIntegerFieldUpdaterDemo<DemoData> getUpdater(String fieldName){
+		return AtomicIntegerFieldUpdater.newUpdater(DemoData.class, fieldName);
+	}
+	
+	void doit(){
+		DemoData data = new DemoData();
+		System.out.println("1==> " + getUpdater("value1").getAndSet(data, 10));
+		System.out.println("3==> " + getUpdater("value2").incrementAndSet(data));
+		System.out.println("2==> " + getUpdater("value3").decrementAndSet(data));
+		System.out.println("true==> " + getUpdater("value4").compareAndSet(data, 4, 5));
+	}
+	
+	public static void main(String[] args) {
+		AtomicIntegerFieldUpdaterDemo demo = 
+	
+	}
+
+}
