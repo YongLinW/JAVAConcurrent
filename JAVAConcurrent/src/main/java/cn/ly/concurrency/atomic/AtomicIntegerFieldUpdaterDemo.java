eb25f6ae972f80ea8089ab2e07f98595d45545cc
LinYongW <linyongw@gmail.com>
2014-10-24 09:09:08 +0800

    add ReorderingDemo and modify AtomicIntegerFileldUpdaterDemo

--- JAVAConcurrent/src/main/java/cn/ly/concurrency/atomic/AtomicIntegerFieldUpdaterDemo.java
+++ JAVAConcurrent/src/main/java/cn/ly/concurrency/atomic/AtomicIntegerFieldUpdaterDemo.java
@@ -10,21 +10,21 @@ public class AtomicIntegerFieldUpdaterDemo {
 		protected volatile int value3 = 3;
 		private volatile int value4 = 4;
 	}
-	AtomicIntegerFieldUpdaterDemo<DemoData> getUpdater(String fieldName){
+	AtomicIntegerFieldUpdater<DemoData> getUpdater(String fieldName){
 		return AtomicIntegerFieldUpdater.newUpdater(DemoData.class, fieldName);
 	}
 	
 	void doit(){
 		DemoData data = new DemoData();
 		System.out.println("1==> " + getUpdater("value1").getAndSet(data, 10));
-		System.out.println("3==> " + getUpdater("value2").incrementAndSet(data));
-		System.out.println("2==> " + getUpdater("value3").decrementAndSet(data));
+		System.out.println("3==> " + getUpdater("value2").incrementAndGet(data));
+		System.out.println("2==> " + getUpdater("value3").decrementAndGet(data));
 		System.out.println("true==> " + getUpdater("value4").compareAndSet(data, 4, 5));
 	}
 	
 	public static void main(String[] args) {
-		AtomicIntegerFieldUpdaterDemo demo = 
-	
+		AtomicIntegerFieldUpdaterDemo demo = new AtomicIntegerFieldUpdaterDemo();
+		demo.doit();
 	}
 
 }
---
+++ JAVAConcurrent/src/main/java/cn/ly/concurrency/atomic/ReorderingDemo.java
@@ -0,0 +1,35 @@
+package cn.ly.concurrency.atomic;
+
+public class ReorderingDemo {
+
+	static int x = 0, y = 0, a = 0, b = 0;
+
+	public static void main(String args[]) {
+		for (int i = 0; i < 100; i++) {
+			x = y = a = b = 0;
+			Thread one = new Thread() {
+				public void run() {
+					a = 1;
+					x = b;
+				}
+			};
+
+			Thread two = new Thread() {
+				public void run() {
+					b = 1;
+					y = a;
+				}
+			};
+			one.start();
+			two.start();
+			try {
+				one.join();
+				two.join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			System.out.println(x + " " + y);
+		}
+	}
+}
